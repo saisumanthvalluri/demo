cfa62db96532421b0e4907f8d131292e5dd9a8ac
saisumanth valluri <saisumanth.valluri@infinitylearn.com>
2024-07-30 17:27:16 +0530

    added vercel.json

--- src/Router/MainRouter.js
+++ src/Router/MainRouter.js
@@ -3,16 +3,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "../pages/Auth";
 import ForgotPassword from "../pages/Auth/ForgotPassword";
-import ResetPassword from "../pages/Auth/ResetPassword";
-import LandingPage from "../pages/LandingPage";
-import Jobs from "../pages/Jobs";
+import LandinPage from "../pages/LandingPage";
 import NotFound from "../pages/NotFound";
+import ResetPassword from "../pages/Auth/ResetPassword";
+import Jobs from "../pages/Jobs";
 
 const MainRouter = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                <Route path="/" element={<LandinPage />} />
                 <Route path="/auth" element={<Auth />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/reset-password" element={<ResetPassword />} />
